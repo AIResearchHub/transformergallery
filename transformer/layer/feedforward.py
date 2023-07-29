3d577ae1f216bd04200ef31c780fd788f59010a3
ArjunSohur <105809809+ArjunSohur@users.noreply.github.com>
2023-07-29 16:12:52 -0400

    Update feedforward.py

--- transformer/layer/feedforward.py
+++ transformer/layer/feedforward.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 
 class FeedForward(nn.Module):
     """
-    A simple feed forward network to be used in transformer layers.
+    A simple feed forward network to be used in transformer layers.  Used to prep the multi-head attention output for the next input.
 
     Architecture:
         Sequential(
@@ -24,12 +24,15 @@ class FeedForward(nn.Module):
     def __init__(self, dim, inner_dim):
         super(FeedForward, self).__init__()
 
+        # Simple nn with non-linear activation function in between the two layers
+        # For more on GELU: https://paperswithcode.com/method/gelu#:~:text=The%20GELU%20activation%20function%20is%20x%20%CE%A6%20(%20x%20)%20%2C%20where,of%20as%20a%20smoother%20ReLU.
         self.ff = nn.Sequential(
             nn.Linear(dim, inner_dim),
             nn.GELU(),
             nn.Linear(inner_dim, dim)
         )
 
+    # Sending tensor x of dimension "dim" though the network
     def forward(self, x):
         return self.ff(x)
 
